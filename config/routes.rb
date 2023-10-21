eb079c539453a77a6bfe258df6bd6e3e77a77693
maymine <sm9365277@gmail.com>
2023-10-21 01:40:38 -0500

    maybe the last commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root "pages#home"
   get 'pages/home'
+  post 'tts', to: 'pages#tts', as: :tts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
